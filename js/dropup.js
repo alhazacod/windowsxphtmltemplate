ba58f9062ace96486e8355fdd096a5500a3ef29c
jenkko <angel_01@hotmail.es>
2019-09-25 20:46:18 -0500

    Start menu created.

---
+++ js/dropup.js
@@ -0,0 +1,14 @@
+dropupcontent = document.getElementById('start-content'); //you can change the id to the same id of your dropup content
+
+// close te dropdown when you click out the menu
+window.addEventListener('click', function(e){
+    if(dropupcontent.classList.contains('dropup-show') && !e.target.classList.contains("start-btn") && e.target.id!=dropupcontent.id)
+        toggleUp();
+});
+
+
+function toggleUp(){
+    dropupcontent.classList.toggle("dropup-show");
+}
+
+
