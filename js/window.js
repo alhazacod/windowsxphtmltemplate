1c78c02f676537802d82db19dd832be9fc42128a
jenkko <angel_01@hotmail.es>
2019-09-29 22:08:30 -0500

    Some bugs fixed and resize function started

--- js/window.js
+++ js/window.js
@@ -1,46 +1,60 @@
-windoww = document.getElementById("window")
-dragwindow = document.getElementById("windowTitle")
-window.addEventListener('click', function(e){
-        console.log(e.pageX + "," + e.pageY);
+wwindow = document.querySelectorAll(".window")[0];
+windoww = document.querySelectorAll(".desktop")[0].offsetWidth-1;
+windowh = document.querySelectorAll(".desktop")[0].offsetHeight-45;
+dragrd = wwindow.querySelectorAll(".drag-rd")[0];
+
+dragwindow = wwindow.querySelectorAll(".title")[0];
+  window.addEventListener('click', function(e){
 });
 
 dragwindow.onmousedown = function(event) {
-    console.log("asdf");
-    let shiftX = event.clientX - windoww.getBoundingClientRect().left;
-    let shiftY = event.clientY - windoww.getBoundingClientRect().top;
-  
-    windoww.style.position = 'absolute';
-    windoww.style.zIndex = 1000;
-    document.body.append(windoww);
-    
-    moveAt(event.pageX, event.pageY);
-  
-    // moves the windoww at (pageX, pageY) coordinates
-    // taking initial shifts into account
-    function moveAt(pageX, pageY) {
-      windoww.style.left = pageX - shiftX + 'px';
-      windoww.style.top = pageY - shiftY + 'px';
-    }
-  
-    function onMouseMove(event) {
-        console.log(windoww.style.top);
-        if(event.pageY < 760)
-        moveAt(event.pageX, event.pageY);
-        else
-        moveAt(event.pageX, 760);
-    }
-  
-    // move the windoww on mousemove
-    document.addEventListener('mousemove', onMouseMove);
-  
-    // drop the windoww, remove unneeded handlers
-    windoww.onmouseup = function() {
-      document.removeEventListener('mousemove', onMouseMove);
-      windoww.onmouseup = null;
-    };
-  
+  let shiftX = event.clientX - wwindow.getBoundingClientRect().left;
+  let shiftY = event.clientY - wwindow.getBoundingClientRect().top;
+
+  wwindow.style.zIndex = 10;
+
+  windoww = document.querySelectorAll(".desktop")[0].offsetWidth-1;
+  windowh = document.querySelectorAll(".desktop")[0].offsetHeight-45; 
+
+  function moveAt(pageX, pageY) {
+    wwindow.style.left = pageX - shiftX + 'px';
+    wwindow.style.top = pageY - shiftY + 'px';
+  }
+
+  function onMouseMove(event) {
+      if(event.pageY < windowh && event.pageY > 1 && event.pageX < windoww && event.pageX > 1)
+      moveAt(event.pageX, event.pageY);
+      else{
+        document.removeEventListener('mousemove', onMouseMove);
+        wwindow.onmouseup = null;
+      }
+  }
+
+  // move the wwindow on mousemove
+  document.addEventListener('mousemove', onMouseMove);
+
+  // drop the wwindow, remove unneeded handlers
+  wwindow.onmouseup = function() {
+    wwindow.style.zIndex = 9;
+    document.removeEventListener('mousemove', onMouseMove);
+    wwindow.onmouseup = null;
   };
-  
-  windoww.ondragstart = function() {
-    return false;
+};
+
+dragrd.onmousedown = function(event) {
+  function resize(w, h){
+    console.log(w + ", " + h);
+    wwindow.style.width = w + 'px';
+    wwindow.style.height = h + 'px';
+  }
+
+  function onMouseMove(event){
+    resize(event.clientX - wwindow.getBoundingClientRect().left, event.clientY - wwindow.getBoundingClientRect().top);
+  }
+
+  document.addEventListener('mousemove', onMouseMove);
+
+  wwindow.onmouseup = function(){
+    document.removeEventListener('mousemove', onMouseMove)
   };
+};
