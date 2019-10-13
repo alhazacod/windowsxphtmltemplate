92c5b08e06499f8540f46a65335e02a21cf53a6f
jenkko <angel_01@hotmail.es>
2019-10-13 13:03:54 -0500

    left down resizer works

--- js/window.js
+++ js/window.js
@@ -4,6 +4,7 @@ windowh = document.querySelectorAll(".desktop")[0].offsetHeight-45;
 dragrd = wwindow.querySelectorAll(".drag-rd")[0];
 dragru = wwindow.querySelectorAll(".drag-ru")[0];
 draglu = wwindow.querySelectorAll(".drag-lu")[0];
+dragld = wwindow.querySelectorAll(".drag-ld")[0];
 
 dragwindow = wwindow.querySelectorAll(".title")[0];
   window.addEventListener('click', function(e){
@@ -121,6 +122,38 @@ draglu.onmousedown = function (event) {
   function onMouseMove(event) {
       if (event.pageY < windowh && event.pageY > 1 && event.pageX < windoww && event.pageX > 1)
           resize(right - event.clientX, bottom - event.clientY, event.clientX);
+      else {
+          document.removeEventListener('mousemove', onMouseMove);
+          wwindow.onmouseup = null;
+      }
+  }
+
+  document.addEventListener('mousemove', onMouseMove);
+
+  wwindow.onmouseup = function () {
+      document.removeEventListener('mousemove', onMouseMove);
+      wwindow.onmouseup = null;
+  };
+};
+
+dragld.onmousedown = function (event) {
+  let top = wwindow.getBoundingClientRect().top;
+  let right = wwindow.getBoundingClientRect().right;
+  let left = wwindow.getBoundingClientRect().left;
+
+  function resize(w, h, x) {
+      if (w > 200) {
+          wwindow.style.left = x + 'px';
+          wwindow.style.width = w + 'px';
+      }
+      if (h > 200) {
+          wwindow.style.height = h + 'px';
+      }
+  }
+
+  function onMouseMove(event) {
+      if (event.pageY < windowh && event.pageY > 1 && event.pageX < windoww && event.pageX > 1)
+          resize(right - event.clientX, event.clientY - top, event.clientX);
       else {
           document.removeEventListener('mousemove', onMouseMove);
           wwindow.onmouseup = null;
